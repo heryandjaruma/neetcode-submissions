1508111cf29407d31143ccd55de564b576fac084
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-22 22:04:46 +0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # build a frequency map first
+        map = {} # key -> val    num -> freq
+
+        for num in nums:
+            map[num] = 1 + map.get(num, 0)
+        
+        # then init a list of list where the inner list is which num has that frequency
+        freq = [[] for _ in range(len(nums) + 1)] # +1 because we want to disntict bucket for 0 and for 1
+        for num, count in map.items():
+            freq[count].append(num)
+        
+        # then return top most k value from the biggest which is from the freq arr with the biggest index
+        returnVal = []
+        for i in range(len(freq) - 1, 0, -1): # prevent out of index ; ; goes backwards
+            for num in freq[i]:
+                returnVal.append(num)
+                if len(returnVal) == k:
+                    return returnVal
