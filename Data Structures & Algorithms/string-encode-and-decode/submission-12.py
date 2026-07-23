1fa54f5993026bd7ed3e6be51858f2cfaf20bd37
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-23 21:05:06 +0700

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+class Solution:
+    # thought process
+    # apparently it's fine in encoding, but in decoding process it will
+    # be very much hard to exactly decode it
+    # the right approach is to specify the length and the delimiter
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        if not strs: # special case
+            return ""
+        for word in strs:
+            if word == "":
+                encoded.append("0")
+            else:
+                encoded.append(f"{len(word)}#{word}")
+        return "#".join(encoded)
+
+    def decode(self, s: str) -> List[str]:
+        if s == "": # special case
+            return []
+        result = []
+        # convert into queue
+        squeue = deque(s)
+        while True:
+            # read char until delimiter found
+            # assuming next char is # means it has to be delimiter from previous word
+            if squeue[0] == "#":
+                squeue.popleft()
+            lengthStr = ""
+            while True:
+                nextChar = squeue.popleft()
+                if nextChar in '1234567890':
+                    lengthStr += nextChar
+                if nextChar == "#": # stop reading length
+                    break
+                if not squeue:
+                    break
+            nextword = ""
+            for i in range(int(lengthStr)):
+                nextword += squeue.popleft()
+            result.append(nextword)
+            if not squeue:
+                break
+            # squeue.popleft() # nextchar has to be delimiter so just pop
+        return result
+
