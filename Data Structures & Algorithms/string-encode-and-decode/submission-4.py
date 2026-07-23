e97df4214579ab262dc24371cc61148f444d7b9b
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-23 12:01:20 +0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    # thought process
+    # why don't just append string and decode?
+    # it's hard to decode since it has to carry information
+    # about each string
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        if not strs: # special case
+            return ""
+        if strs and strs[0] == "":
+            return "0"
+        for word in strs:
+            encoded.append(f"{len(word)}{word}")
+        return "\x00".join(encoded)
+
+    def decode(self, s: str) -> List[str]:
+        if s == "": # special case
+            return []
+        if s == "0":
+            return [""]
+        delimited = s.split("\x00")
+        strs = []
+        for decoded in delimited:
+            charLen = decoded[0]
+            word = decoded[1:]
+            strs.append(word)
+        return strs
+
