874b4450db010115dc5aa32c7243976451cd2974
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-26 22:30:47 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        left = 0 
+        right = -1
+        
+        while left < abs(right):
+            print(s[left], s[right])
+            
+            while True:
+                if s[left] in valid:
+                    break
+                else:
+                    left += 1
+            
+            while True:
+                if s[right] in valid:
+                    break
+                else:
+                    right += 1
+            
+            if s[left].loweR() == s[right].lower():
+                left += 1 
+                right -= 1
+                continue
+            else:
+                return False
+        return True
+
