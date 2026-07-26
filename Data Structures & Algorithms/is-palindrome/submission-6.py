02f98fdc75d7c69f44a595c2740fec507dc87ea6
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-26 22:39:34 +0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+        
+        # strip space
+        s = s.strip()
+        # replace all spaces
+        s = s.replace(" ", "")
+
+        left = 0 
+        right = len(s) - 1
+        
+        while left < right:
+            print(f"L {left} {s[left]}  R {right} {s[right]}")
+            
+            while True:
+                if s[left] in valid:
+                    break
+                else:
+                    left += 1
+            
+            while True:
+                if s[right] in valid:
+                    break
+                else:
+                    right -= 1
+            
+            if s[left].lower() == s[right].lower():
+                left += 1 
+                right -= 1
+                continue
+            else:
+                return False
+        return True
+
