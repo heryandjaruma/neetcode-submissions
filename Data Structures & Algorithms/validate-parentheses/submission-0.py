b4c05ad6ae719be1bc991d7f85ec44b457457465
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-07-30 09:14:13 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        # thoughts
+        # - to use stack to track the valid opening and closing parentheses
+
+        stack = deque()
+
+        # special case
+        if len(s) == 1:
+            return False
+        
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.append(char)
+            else:
+                # check if it matches the top most on stack
+                top = stack.pop()
+                if char == ')' and top != '(':
+                    return False
+                if char == '}' and top != '{':
+                    return False
+                if char == ']' and top != '[':
+                    return False
+        
+        if stack: # still have something in stack
+            return False
+        
+        return True
+        
