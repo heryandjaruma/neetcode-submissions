2bc45705d1555591ad5b621a99abeba55371b7ec
Heryan Djaruma <86642592+heryandjaruma@users.noreply.github.com>
2026-08-03 20:38:58 +0800

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # thoughts:
+        # - there has to be 2 outer most element, and between
+        # is most volume it can contain
+        # - what is water volume?
+        #   - maybe it's just a square equation
+        # - to define the outer most 'pillars'
+        #   - index matters: bigger span better
+        #   - height matters: taller better
+
+        # to get data
+        # two approach:
+        # - is to double loop and compare one to other
+        # element each O(n2)
+        # - use 2 pointers left and right to find
+        # current most until in the middle
+
+        # - but how do i keep the most while still finding
+        # the bst
+        # - actually since we only care about the most volume
+        # and not index, then just calculate current span
+        # times height
+        # advances left and right one at time
+        # ^ but this approach is wrong because it assumes that
+        # value converges at the middle, while the most calculated
+        # can be in between
+    
+        # index
+        l = 0
+        r = len(heights) - 1
+        # index to add
+        leftTurn = True # first to advanced before right
+        mostWater = 0
+        while l < r:
+            currentMost = (r-l) * min(heights[l], heights[r])
+            if currentMost > mostWater:
+                mostWater = currentMost
+            
+            if leftTurn:
+                l += 1
+                leftTurn = False
+            else:
+                r -= 1
+                leftTurn = True
+            
+        return mostWater
